30fd404b81bec3a7967b35bf01234ef6b902c7a8
Curo <33403861+cheongan@users.noreply.github.com>
2019-11-09 21:51:33 +0900

    postman 추가
    pause도 없애버림

--- AIP.bat
+++ AIP.bat
@@ -1,12 +1,12 @@
 wget -O %systemdrive%//Users//%username%//Desktop//WhaleSetup.exe http://update.whale.naver.net/downloads/installers/WhaleSetup.exe
 
-pause
-
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 choco install nodejs-lts -y
 
 choco install vscode -y
+
+choco install postman -y
 
 choco install git -y
 
