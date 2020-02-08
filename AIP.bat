c1d9a1f856221090e8ff3151b39af6fc5c64f023
Curo <33403861+cheongan@users.noreply.github.com>
2020-02-08 17:30:06 +0900

    SSL 오류 해결

--- AIP.bat
+++ AIP.bat
@@ -1,6 +1,6 @@
 wget -O %systemdrive%//Users//%username%//Desktop//WhaleSetup.exe http://update.whale.naver.net/downloads/installers/WhaleSetup.exe
 
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 choco install nodejs-lts -y
 
@@ -9,4 +9,3 @@ choco install vscode -y
 choco install postman -y
 
 choco install git -y
-
