be5776963ca73f77816753b3585d5def79c035fd
Curo <33403861+Curo-Dev@users.noreply.github.com>
2023-11-10 15:55:45 +0900

    Update AIP.bat

--- AIP.bat
+++ AIP.bat
@@ -3,7 +3,4 @@ wget -O %systemdrive%//Users//%username%//Desktop//WhaleSetup.exe http://update.
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 choco install nodejs-lts -y
-
 choco install git -y
-
-choco install github-desktop -y
